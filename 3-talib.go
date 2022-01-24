2e48cad0234582e9d03edc12f81174cbd7d26abb
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-24 16:26:34 +0900

    talibに関する記述

---
+++ 3-talib.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
+)
+
+func main() {
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
+	mva := talib.Ema(spy.Close, 20)
+	fmt.Println(mva)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module ThirdPartyPack
 go 1.17
 
 require (
+	github.com/markcheno/go-quote v0.0.0-20211116021555-bb34b59a97eb // indirect
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	gopkg.in/ini.v1 v1.66.3 // indirect
 )
